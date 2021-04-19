da68bff70079a80250c4e0aa56d922bcc2859009
Pashayam <42655221+Pashayam@users.noreply.github.com>
2021-04-19 19:59:48 +0300

    Update test_one_hot_encoder.py

--- one_hot_encoder/test_one_hot_encoder.py
+++ one_hot_encoder/test_one_hot_encoder.py
@@ -1,6 +1,5 @@
 import unittest
 import one_hot_encoder
-import pytest
 
 
 class TestTF(unittest.TestCase):
@@ -16,24 +15,18 @@ class TestTF(unittest.TestCase):
         ]
         self.assertEqual(actual, expected)
 
-    def test_cities_without_copy(self):
-        cities = ['Moscow', 'New York', 'Moscow']
-        actual = one_hot_encoder.fit_transform(cities)
-        expected = [
-            ('Moscow', [0, 1]),
-            ('New York', [1, 0]),
-            ('Moscow', [0, 1]),
-        ]
-        self.assertEqual(actual, expected)
-
     def test_not_london(self):
         cities = ['Moscow', 'New York']
         actual = one_hot_encoder.fit_transform(cities)
         self.assertNotIn(('London', [1, 0]), actual)
 
-    def test_exception(self):
-        with self.assertRaises(Exception):
-            one_hot_encoder.fit_transform(342)
+    def test_not_arg(self):
+        with self.assertRaises(TypeError):
+            one_hot_encoder.fit_transform()
+
+    def test_int_arg(self):
+        with self.assertRaises(TypeError):
+            one_hot_encoder.fit_transform(423)
 
 
 if __name__ == '__main__':
